352b596e6d21b8f9e6901b7abf2082012367002b
Zeke-Y-Liu <Zeke-Y-Liu@gmail.com>
2013-08-13 22:32:37 +0800

    Commit: initial classes

---
+++ Ranger/src/com/ranger/collector/Version.java
@@ -0,0 +1,16 @@
+package com.ranger.collector;
+
+public class Version {
+
+	private String _version;
+
+	public String getVersion() {
+		return _version;
+	}
+
+	public void setVersion(String version) {
+		this._version = version;
+	}
+	
+	
+}
---
+++ Ranger/src/com/ranger/model/People.java
@@ -0,0 +1,33 @@
+package com.ranger.model;
+
+public class People {
+
+	private String _name;
+	private String _age;
+	private String _mailBox;
+
+	public String getName() {
+		return _name;
+	}
+
+	public void setName(String name) {
+		this._name = name;
+	}
+
+	public String getAge() {
+		return _age;
+	}
+
+	public void setAge(String age) {
+		this._age = age;
+	}
+
+	public String getMailBox() {
+		return _mailBox;
+	}
+
+	public void setMailBox(String mailBox) {
+		this._mailBox = mailBox;
+	}
+
+}
