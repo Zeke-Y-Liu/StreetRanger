8e8a8f37b58eb656c1fb45ad2cbd2ef1807f4ccd
Zeke-Y-Liu <Zeke-Y-Liu@gmail.com>
2013-09-18 22:56:54 +0800

    Add getPublicTimeline test case.

---
+++ Ranger/weibo4j/src/test/java/weibo4j/GetPublicTimelineTest.java
@@ -0,0 +1,35 @@
+package weibo4j;
+
+import org.testng.annotations.Test;
+
+import weibo4j.model.Status;
+import weibo4j.model.StatusWapper;
+import weibo4j.model.WeiboException;
+import weibo4j.util.Log;
+
+
+
+public class GetPublicTimelineTest {
+
+	@Test
+	public void testGetPublicTimeline() {
+		String access_token = "2.00d5evFEnhnsoBc45cd025fencuzbE";//get token code from <code>OAuth4Code.java</code>
+		Timeline tm = new Timeline();
+		tm.client.setToken(access_token);
+		try {
+			StatusWapper status = tm.getPublicTimeline(200,0);//max count is 200
+			for(Status s : status.getStatuses()){
+//				Log.logInfo(s.toString());
+				Log.logInfo(s.getUser().toString());
+
+			}
+			System.out.println(status.getNextCursor());
+			System.out.println(status.getPreviousCursor());
+			System.out.println(status.getTotalNumber());
+			System.out.println(status.getHasvisible());
+		} catch (WeiboException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
